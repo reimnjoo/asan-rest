80bf4daaaac4a0e5f98f3422be8ce2f2d194341a
reimnjoo <aladiahfulminar2@gmail.com>
2024-04-24 15:52:36 +0800

    feat: added seed initializers

---
+++ database/seeders/WarehouseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Warehouse;
+
+class WarehouseSeeder extends Seeder
+{
+    public function run()
+    {
+        Warehouse::factory()->count(10)->create();
+    }
+}
