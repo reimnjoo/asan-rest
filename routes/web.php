610d4318c6ab4ccd5f560ff8c6bf1512ef25cb1c
reimnjoo <aladiahfulminar2@gmail.com>
2024-05-03 00:48:50 +0800

    added crsf route

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test']);
+});
+
+Route::get('/csrf-cookie', function () {
+    return response()->json(['csrfToken' => csrf_token()]);
+});
+
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
