1c67608282a59a959c46009aa8af76ac088af249
reimnjoo <aladiahfulminar2@gmail.com>
2024-05-03 16:09:10 +0800

    staged: generated and staged initial configs

---
+++ app/Http/Controllers/ScrapdataController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Scrapdata;
+use App\Http\Requests\StoreScrapdataRequest;
+use App\Http\Requests\UpdateScrapdataRequest;
+
+class ScrapdataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreScrapdataRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Scrapdata $scrapdata)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Scrapdata $scrapdata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateScrapdataRequest $request, Scrapdata $scrapdata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Scrapdata $scrapdata)
+    {
+        //
+    }
+}
